a85fe8361858f49b9dad07ee8589251201189af2
Rob Dobson <rob@rdobson.co.uk>
2013-10-10 13:19:32 +0100

    Updating view to render a jinja2 template based on multiple data sources (page + repo).
    Signed-off-by: Rob Dobson <rob@rdobson.co.uk>

--- generate/views.py
+++ generate/views.py
@@ -3,21 +3,24 @@
 from django.views import generic
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse
+import jinja2
 
 from articlegenerator.extract import *
 
 from models import *
+from templates.models import Template
 
 REPO_BASE_LOC = '/tmp/'
 
-def get_driver_repo_data(location):
+def get_driver_repo_data_source(location):
     data_source = DriverRepoDataSource(REPO_BASE_LOC + location)
     data_source.collect()
-    return data_source.export() 
+    return data_source 
 
 class PageCreate(generic.edit.CreateView):
     model = Page
-    fields = ['name', 'location', 'template']
+    fields = ['name', 'location', 'template', 'xs_version', 'vendor_name',
+              'supp_pack_guide_ctx']
     template_name_suffix = '_create'
 
 class PageUpdate(generic.edit.UpdateView):
@@ -42,6 +45,27 @@ class PageRender(generic.base.View):
     
     def get(self, request, pk):
         page = Page.objects.get(pk=pk)
-        data = get_driver_repo_data(page.location)
+        page_rec = { 
+                        'xs_version'            : page.xs_version,
+                        'vendor_name'           : page.vendor_name,
+                        'supp_pack_guide_ctx'   : page.supp_pack_guide_ctx,
+                        'hotfix_name'           : page.hotfix_name,
+                        'hotfix_ctx'            : page.hotfix_ctx,
+                        'original_ctx'          : page.original_ctx,
+                   }
+        page_data = ArticleDataSource(data=page_rec)
+        repo_data = get_driver_repo_data_source(page.location)
 
-        return HttpResponse('Test: %s' % data)
+        template = Template.objects.get(pk=page.template.pk)
+        jinja_template = jinja2.Template(template.data)
+
+        render_data = page_data + repo_data
+        return HttpResponse(jinja_template.render(**render_data.export()))
+
+
+
+
+
+
+
+
