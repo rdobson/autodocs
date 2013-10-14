8b2a935cc5e2ce1958b2ec840d63ad509fb457e3
Rob Dobson <rob@rdobson.co.uk>
2013-10-14 13:09:16 +0100

    Renaming page to 'article' in order to save confusion. The Article object contains the mapping between data and a template.
    Signed-off-by: Rob Dobson <rob@rdobson.co.uk>

--- generate/urls.py
+++ generate/urls.py
@@ -4,10 +4,10 @@ from generate import views
 
 urlpatterns = patterns('',
     
-    url(r'^$', views.PageList.as_view(), name='page-list'),
-    url(r'(?P<pk>\d+)/render/$', views.PageRender.as_view(), name='page-render'),
+    url(r'^$', views.ArticleList.as_view(), name='article-list'),
+    url(r'(?P<pk>\d+)/render/$', views.ArticleRender.as_view(), name='article-render'),
 
-    url(r'add/$', views.PageCreate.as_view(), name='page_create'),
-    url(r'(?P<pk>\d+)/$', views.PageUpdate.as_view(), name='page_update'),
-    url(r'(?P<pk>\d+)/delete/$', views.PageDelete.as_view(), name='page_delete'),
+    url(r'add/$', views.ArticleCreate.as_view(), name='article_create'),
+    url(r'(?P<pk>\d+)/$', views.ArticleUpdate.as_view(), name='article_update'),
+    url(r'(?P<pk>\d+)/delete/$', views.ArticleDelete.as_view(), name='article_delete'),
 )
--- generate/views.py
+++ generate/views.py
@@ -17,49 +17,49 @@ def get_driver_repo_data_source(location):
     data_source.collect()
     return data_source 
 
-class PageCreate(generic.edit.CreateView):
-    model = Page
+class ArticleCreate(generic.edit.CreateView):
+    model = Article
     fields = ['name', 'location', 'template', 'xs_version', 'vendor_name',
               'supp_pack_guide_ctx']
     template_name_suffix = '_create'
 
-class PageUpdate(generic.edit.UpdateView):
-    model = Page
+class ArticleUpdate(generic.edit.UpdateView):
+    model = Article
     fields = ['name', 'location', 'template']
     template_name_suffix = '_update'
 
-class PageDelete(generic.edit.DeleteView):
-    model = Page
+class ArticleDelete(generic.edit.DeleteView):
+    model = Article
     fields = ['name', 'location', 'template']
     template_name_suffix = '_check_delete'
-    success_url = reverse_lazy('generate:page-list')
+    success_url = reverse_lazy('generate:article-list')
 
-class PageList(generic.ListView):
-    context_object_name = 'page_list'
+class ArticleList(generic.ListView):
+    context_object_name = 'article_list'
         
     def get_queryset(self):
-        return Page.objects.all()
+        return Article.objects.all()
 
 
-class PageRender(generic.base.View):
+class ArticleRender(generic.base.View):
     
     def get(self, request, pk):
-        page = Page.objects.get(pk=pk)
-        page_rec = { 
-                        'xs_version'            : page.xs_version,
-                        'vendor_name'           : page.vendor_name,
-                        'supp_pack_guide_ctx'   : page.supp_pack_guide_ctx,
-                        'hotfix_name'           : page.hotfix_name,
-                        'hotfix_ctx'            : page.hotfix_ctx,
-                        'original_ctx'          : page.original_ctx,
+        article = Article.objects.get(pk=pk)
+        article_rec = { 
+                        'xs_version'            : article.xs_version,
+                        'vendor_name'           : article.vendor_name,
+                        'supp_pack_guide_ctx'   : article.supp_pack_guide_ctx,
+                        'hotfix_name'           : article.hotfix_name,
+                        'hotfix_ctx'            : article.hotfix_ctx,
+                        'original_ctx'          : article.original_ctx,
                    }
-        page_data = ArticleDataSource(data=page_rec)
-        repo_data = get_driver_repo_data_source(page.location)
+        article_data = ArticleDataSource(data=article_rec)
+        repo_data = get_driver_repo_data_source(article.location)
 
-        template = Template.objects.get(pk=page.template.pk)
+        template = Template.objects.get(pk=article.template.pk)
         jinja_template = jinja2.Template(template.data)
 
-        render_data = page_data + repo_data
+        render_data = article_data + repo_data
         return HttpResponse(jinja_template.render(**render_data.export()))
 
 
